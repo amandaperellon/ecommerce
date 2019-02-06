1bc87569d82c85ebb95e888a09844360761b1f10
Amanda Perellon <amanda.perellon@gmail.com>
2019-02-06 14:48:55 -0200

    tela de login

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
+session_start();
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -24,9 +27,41 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
 
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+
+$app->get('/admin/login', function() {
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /curso1/ECommerce/index.php/admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /curso1/ECommerce/index.php/admin/login");
+	exit;
 
 });
 
---
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Hcode;
+
+class Model{
+	private $values = [];
+
+	public function  __call($name, $args){
+
+		$method = substr($name, 0, 3);  //posição 0 a 3, ou seja, traga 0,1 e 2, é quantidade
+
+		$fieldName = substr($name, 3, strlen($name));
+
+		switch ($method) {
+			case 'get':
+			
+				return $this->values[$fieldName]; 
+			
+			break;
+			
+			case 'set':
+				return $this->values[$fieldName] = $args[0];
+			break;
+		}
+	}
+
+	public function setData($data = array()){
+
+		foreach ($data as $key => $value) {
+			
+			$this->{"set" . $key}($value);
+
+		}
+
+	}
+
+	public function getValues(){
+
+		return $this->values;
+
+	}
+}
+
+?>
