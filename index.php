dea7cdfc67fa6d1e2c0a5a9ffaba12cf5a6251bb
Amanda Perellon <amanda.perellon@gmail.com>
2019-02-11 14:55:04 -0200

    Lista de Produtos - Site

--- index.php
+++ index.php
@@ -23,6 +23,8 @@ require_once("admin-categories.php");
 
 require_once("admin-products.php");
 
+require_once("functions.php");
+
 $app->run();
 
  ?>
--- site.php
+++ site.php
@@ -1,14 +1,19 @@
 <?php
 
 use \Hcode\Page;
+use \Hcode\Model\Products;
 
 $app->config('debug', true);
 
 $app->get('/', function() {
 
+	$products = Products::listAll();
+
 	$page = new Page();
 
-	$page->setTpl("index");
+	$page->setTpl("index", [
+		'products'=>Products::checkList($products)
+	]);
 
 }); 
 ?>
