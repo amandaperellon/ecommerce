7f3319e01f7522ece6ed767f3840b0fad2116d45
Amanda Perellon <amanda.perellon@gmail.com>
2019-02-18 14:08:50 -0300

    Busca e pagination de categorias - Admin

--- admin-categories.php
+++ admin-categories.php
@@ -10,12 +10,40 @@ $app->get('/admin/categories', function(){
 
 	User::verifyLogin();
 
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+	if ($search != '') {
+		
+		$pagination = Category::getPageSearch($search, $page, 10);
+
+	}else{
+
+		$pagination = Category::getPage($page, 10);
+	}
+
+	$pages = [];
+
+	for ($x=0; $x < $pagination['pages']; $x++) { 
+		
+		array_push($pages, [
+			'href'=>'/curso1/ECommerce/index.php/admin/categories?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+		]),
+		'text'=>$x+1
+		]);
+	}
+
 	$categories = Category::listAll();
 
 	$page = new PageAdmin();
 
 	$page->setTpl("categories", [
-		"categories"=>$categories
+		"categories"=>$pagination['data'],
+		"search"=>$search,
+		"pages"=>$pages
 	]);
 
 });
